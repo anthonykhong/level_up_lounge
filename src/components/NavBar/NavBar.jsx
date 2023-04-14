f2d689e21219e4967ae26b48416808d377d01620
anthonykhong <anthonykhong95200@gmail.com>
2023-04-14 05:39:53 +0200

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
-export default function NavBar() {
+export default function NavBar({ user }) {
   return (
-    <div>
-      <nav>NavBar</nav>
-    </div>
+    <nav className="NavBar">
+      <img src="./public/images/logo.png" />
+      &nbsp; &nbsp;
+      <Link to="/">Posts</Link>
+      &nbsp; &nbsp;
+      <Link to="/new">Create Posts</Link>
+    </nav>
   );
 }
