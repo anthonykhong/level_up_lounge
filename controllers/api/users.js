bbf368d50d09c1fea5bbb0839b86115956958968
anthonykhong <anthonykhong95200@gmail.com>
2023-04-14 07:47:06 +0200

    render routes on server

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -26,7 +26,7 @@ async function login(req, res) {
     const token = createJWT(user);
     res.json(token);
   } catch (error) {
-    res.status(400).json(error);
+    res.status(500).json(error);
   }
 }
 
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 require("dotenv").config();
-// require('./config/database')
+require("./config/database");
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
+
+app.use("/api/users", require("./routes/api/users"));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
