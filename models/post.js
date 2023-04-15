3ef5420df9d2bde131a9ce0c8063a23903e3a99c
anthonykhong <anthonykhong95200@gmail.com>
2023-04-15 20:42:44 +0200

    implement comment schema

--- models/post.js
+++ models/post.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    userName: String,
+    userAvatar: String,
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const postSchema = new Schema(
   {
     title: {
