3fbf1158ebeb106c6942cdc1f1a9f78cade1977f
anthonykhong <anthonykhong95200@gmail.com>
2023-04-15 04:54:06 +0200

    style blog page

--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function BlogPage() {
   return (
-    <div>
-      <h1>BlogPage</h1>
+    <div className="flex justify-center items-center m-10">
+      <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-500 to-blue-600 text-5xl">
+        All Posts
+      </h1>
     </div>
   );
 }
