403220599f9d6ce435c08235dc7ea46b18fa1a69
anthonykhong <anthonykhong95200@gmail.com>
2023-04-16 04:53:13 +0200

    export functions from posts-api

--- src/utilities/posts-api.js
+++ src/utilities/posts-api.js
@@ -2,6 +2,22 @@ import sendRequest from "./send-request";
 
 const BASE_URL = "/api/posts";
 
+export function allPost() {
+  return sendRequest(`${BASE_URL}`);
+}
+
+export function getPost(id) {
+  return sendRequest(`${BASE_URL}/${id}`);
+}
+
 export function createPost(postData) {
-  return sendRequest(BASE_URL, "POST", postData);
+  return sendRequest(`${BASE_URL}`, "POST", postData);
+}
+
+export function deletePost() {
+  return sendRequest(`${BASE_URL}/${id}`);
+}
+
+export function updatePost() {
+  return sendRequest(`${BASE_URL}/${id}`);
 }
