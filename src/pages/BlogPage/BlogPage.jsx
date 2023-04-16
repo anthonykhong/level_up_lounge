246acfb1f42b6bc3023c9b5224e29164fc15f743
anthonykhong <anthonykhong95200@gmail.com>
2023-04-16 06:23:21 +0200

    implement styling

--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as postsAPI from "../../utilities/posts-api";
 import NewBlog from "../NewBlog/NewBlog";
+import "./BlogPage.css";
 
 export default function BlogPage({ user, setUser }) {
   const navigate = useNavigate();
@@ -11,13 +12,16 @@ export default function BlogPage({ user, setUser }) {
   }
 
   return (
-    <div className="flex justify-center items-center flex-col m-10">
-      <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-500 to-blue-600 text-5xl">
-        All Posts
-      </h1>
-      <div className="py-8 ml-auto">
+    <div>
+      <div>
+        <img className="w-full h-24" src="../../public/images/banner.png" />
+      </div>
+      <div className="flex justify-center items-center flex-row py-4 bg-neutral-800">
+        <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-600 text-5xl pr-6">
+          All Posts
+        </h1>
         <button
-          className="px-4 py-2 rounded-md bg-gradient-to-br from-purple-300 to-blue-600 text-white font-medium"
+          className="px-4 py-2 rounded-md bg-gradient-to-br from-purple-600 to-cyan-600 text-white font-medium "
           onClick={handleCreatePostClick}
         >
           Create Post
