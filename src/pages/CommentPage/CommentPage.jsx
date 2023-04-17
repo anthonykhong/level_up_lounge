f080466d8f66385e7a595741064e5c304a6bea72
anthonykhong <anthonykhong95200@gmail.com>
2023-04-18 01:38:29 +0200

    add functionalities to comment page

--- src/pages/CommentPage/CommentPage.jsx
+++ src/pages/CommentPage/CommentPage.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import NewComment from "../NewComment/NewComment";
 import CommentList from "../../components/CommentList/CommentList";
+import * as commentsAPI from "../../utilities/comments-api";
 
-export default function CommentPage() {
+export default function CommentPage({ user, post }) {
+  const [comments, setComments] = useState([]);
+
+  const addCommentHandler = async (commentData) => {
+    console.log(commentData);
+    try {
+      const newComment = await commentsAPI.addComment(post._id, commentData);
+      setComments((prevComments) => [...prevComments, newComment]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <>Comment Section</>
-      <CommentList />
-      <NewComment />
+      <CommentList user={user} comments={comments} setComments={setComments} />
+      <NewComment addCommentHandler={addCommentHandler} />
     </div>
   );
 }
