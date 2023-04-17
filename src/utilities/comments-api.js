7772847325e0e4dd26a3e0a32565791f74873861
anthonykhong <anthonykhong95200@gmail.com>
2023-04-18 01:29:09 +0200

    add comment request utility

---
+++ src/utilities/comments-api.js
@@ -0,0 +1,12 @@
+import sendRequest from "./send-request";
+
+const BASE_URL = "/api/posts";
+
+export function addComment(postId, commentData) {
+  console.log(postId);
+  return sendRequest(`${BASE_URL}/${postId}/comments`, "POST", commentData);
+}
+
+export function deleteComment(postId, commentId) {
+  return sendRequest(`${BASE_URL}/${postId}/comments/${commentId}`, "DELETE");
+}
