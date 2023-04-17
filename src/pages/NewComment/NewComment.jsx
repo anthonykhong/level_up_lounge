f06a0b2b198c296598fc1cbf000fb5c77f0f8f85
anthonykhong <anthonykhong95200@gmail.com>
2023-04-18 01:40:31 +0200

    implement functions in newcomment component

--- src/pages/NewComment/NewComment.jsx
+++ src/pages/NewComment/NewComment.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import NewCommentForm from "../../components/NewCommentForm/NewCommentForm";
 
-export default function NewComment() {
+export default function NewComment({ addCommentHandler }) {
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await addCommentHandler({ text: comment });
+      setComment("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  };
   return (
     <div>
-      <NewCommentForm />
+      <NewCommentForm
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        comment={comment}
+      />
     </div>
   );
 }
