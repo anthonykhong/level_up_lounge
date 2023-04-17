f9f30608d91cadd24698e76d2b0f5b9df0207f98
anthonykhong <anthonykhong95200@gmail.com>
2023-04-17 05:36:04 +0200

    create fetch function to display posts

--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import * as postsAPI from "../../utilities/posts-api";
-import NewBlog from "../NewBlog/NewBlog";
 import PostsList from "../../components/PostsList/PostsList";
 import "./BlogPage.css";
 
 export default function BlogPage({ user, setUser }) {
   const navigate = useNavigate();
-  const [posts, setPosts] = useState([
-    { title: "Fire Emblem", content: "Awesome" },
-    { title: "Pokemon", content: "Awesome" },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  async function fetchPosts() {
+    const allPosts = await postsAPI.allPosts();
+    setPosts(allPosts);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   function handleCreatePostClick() {
     navigate("/posts/new");
@@ -22,15 +27,16 @@ export default function BlogPage({ user, setUser }) {
         <img className="w-full h-24" src="../../public/images/banner.png" />
       </div>
       <div className="flex justify-center items-center flex-row py-4 bg-neutral-800">
-        <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-600 text-5xl pr-6">
+        <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-600 to-blue-600 text-5xl">
           All Posts
         </h1>
-        <button
-          className="px-4 py-2 rounded-md bg-gradient-to-br from-purple-600 to-cyan-600 text-white font-medium "
+      </div>
+      <div className="flex justify-center mt-10">
+        <input
+          className="rounded-lg p-2 bg-gray-500"
+          placeHolder="Create Post.."
           onClick={handleCreatePostClick}
-        >
-          Create Post
-        </button>
+        ></input>
       </div>
       <PostsList posts={posts} />
     </div>
