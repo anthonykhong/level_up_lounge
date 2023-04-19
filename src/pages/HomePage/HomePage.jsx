557dcfc2dea1fcd85a9784a93f13b8de392be16b
anthonykhong <anthonykhong95200@gmail.com>
2023-04-19 23:31:11 +0200

    create fetch function to fetch game data

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,35 +1,25 @@
 import React, { useState, useEffect } from "react";
+import * as gamesAPI from "../../utilities/games-api";
+import GameList from "../GameList/GameList";
 
 export default function HomePage() {
   const [games, setGames] = useState([]);
 
   useEffect(() => {
-    async function getGames() {
-      const gameData = await fetch(
-        "http://www.freetogame.com/api/games?platform=pc"
-      );
-      const gamesJson = await gameData.json();
-      console.log(gamesJson);
-      setGames(gamesJson);
+    async function fetchGames() {
+      const games = await gamesAPI.getGames();
+      setGames(games);
     }
-    getGames();
+    fetchGames();
   }, []);
 
   return (
-    <div className="flex justify-center items-center m-10">
+    <div className="flex justify-center flex-col items-center m-10">
       <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
         Level Up Lounge
       </h1>
-
       <div className="my-5">
-        <ul>
-          {games.map((game) => (
-            <li key={game.id}>
-              <h3>{game.title}</h3>
-              <img src={game.thumbnail} alt={game.title} />
-            </li>
-          ))}
-        </ul>
+        <GameList games={games} />
       </div>
     </div>
   );
