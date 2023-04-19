e741eefeb8534822e80c6de7a06a0c96584004a6
anthonykhong <anthonykhong95200@gmail.com>
2023-04-19 23:18:37 +0200

    require seed route in server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const logger = require("morgan");
 require("dotenv").config();
 require("./config/database");
 const http = require("http");
+const Games = require("./models/game");
 const app = express();
 
 app.use(logger("dev"));
@@ -13,6 +14,16 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.use(require("./config/checkTokens"));
 
 app.use("/api/users", require("./routes/api/users"));
+
+app.get("/api/games", async (req, res) => {
+  try {
+    const games = await Games.find();
+    res.json(games);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
 const ensureLoggedIn = require("./config/ensureLoggedIn");
 app.use("/api/posts/", ensureLoggedIn, require("./routes/api/comments"));
