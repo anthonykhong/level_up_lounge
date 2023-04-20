5e4ff15b260351a949f91d372323e3e8bc4b8c34
anthonykhong <anthonykhong95200@gmail.com>
2023-04-20 04:28:07 +0200

    style edit page

--- src/components/EditBlog/EditBlog.jsx
+++ src/components/EditBlog/EditBlog.jsx
@@ -43,28 +43,46 @@ export default function EditBlog() {
           Edit Post
         </h1>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={post.title}
-            onChange={handleChange}
-          />
+      <div className="bg-neutral-800 rounded-lg shadow-md border border-gray-200 p-8 my-20 md:mx-28 flex flex-col">
+        <div className="flex justify-center">
+          <form onSubmit={handleSubmit} className="w-full">
+            <div className="mb-4">
+              <label htmlFor="title" className="text-white">
+                Title:
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={post.title}
+                onChange={handleChange}
+                className="font-display font-bold w-full px-3 py-4 rounded-lg bg-neutral-800 text-white focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="content" className="text-white">
+                Content:
+              </label>
+              <textarea
+                id="content"
+                name="content"
+                value={post.content}
+                onChange={handleChange}
+                className="text-gray-400 w-full px-3 pb-36 rounded-lg bg-neutral-800 focus:outline-none focus:shadow-outline"
+              ></textarea>
+            </div>
+            <hr />
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="text-white rounded-lg bg-purple-500 hover:bg-purple-600 p-2 mt-6"
+              >
+                Save Changes
+              </button>
+            </div>
+          </form>
         </div>
-        <div>
-          <label htmlFor="content">Content:</label>
-          <textarea
-            id="content"
-            name="content"
-            value={post.content}
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <button type="submit">Save Changes</button>
-      </form>
+      </div>
     </div>
   );
 }
