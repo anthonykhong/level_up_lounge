909356ad70e0ed9811b6479635de11255507104b
anthonykhong <anthonykhong95200@gmail.com>
2023-04-21 03:44:35 +0200

    change game title colors

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -5,7 +5,9 @@ export default function GameCard({ game }) {
     <div className="flex flex-row mx-10 mb-10 border border rounded bg-neutral-800">
       <img src={game.thumbnail} alt={game.title} />
       <div className="text-white p-8">
-        <h3 className="font-display font-bold text-xl mb-2">{game.title}</h3>
+        <h3 className="font-display animate-text bg-gradient-to-r from-red-700 via-purple-700 to-rose-800 bg-clip-text text-transparent text-xl mb-2">
+          {game.title}
+        </h3>
         <p>{game.short_description}</p>
         <div className="my-2">
           <p>Genre: {game.genre}</p>
